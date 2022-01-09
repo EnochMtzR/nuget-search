bcb4bf032d83927b207a8779de85f75794d90568
Enoc Martinez Rojas <josnocpp@protonmail.com>
2022-01-08 18:21:27 -0600

    Added description column.

--- nuget-search.py
+++ nuget-search.py
@@ -5,8 +5,15 @@ from tabulate import tabulate
 import re
 import sys
 
+sys.path.append(".")
+
+from table import Table
+
 def get_title(package):
     return package.find_all("a", {"class": "package-title"})[0].text
+
+def get_details(package):
+    return package.find_all("div", {"class": "package-details"})[0].text.strip()
 
 def get_version(package):
     version_container = package.find_all("span", {"class": "icon-text"})[2]
@@ -15,7 +22,8 @@ def get_version(package):
 def get_authors(package):
     container = package.find_all("span", {"class": "package-by"})[0]
     author_containers = container.find_all("a")
-    return list(map(lambda author_container: author_container.text, author_containers))
+    authors = list(map(lambda author_container: author_container.text, author_containers))
+    return ' '.join(authors)
 
 def get_downloads(package):
     downloads_container = package.find_all("span", {"class": "icon-text"})[0]
@@ -24,6 +32,7 @@ def get_downloads(package):
 def get_package(packageContainer):
     return [
         get_title(packageContainer),
+        get_details(packageContainer),
         get_version(packageContainer),
         get_authors(packageContainer),
         get_downloads(packageContainer)
@@ -34,7 +43,8 @@ def get_packages(package_containers):
     return list(map(lambda container: get_package(container), package_containers))
 
 def print_page(packages, page):
-    print(tabulate(packages, headers=["Title", "Version", "Authors", "Downloads"]))
+    packages.insert(0, ["Title", "Details", "Version", "Authors", "Downloads"])
+    print(Table(packages, 28, True))
 
 page = 1
 search = sys.argv[1]
